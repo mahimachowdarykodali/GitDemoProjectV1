7b652ba8c2f1176f0121a0b6bc17e07120e7acba
Mahima <mahimachowdary587@gmail.com>
2023-12-31 14:18:40 -0600

    added

--- src/Test1.java
+++ src/Test1.java
@@ -5,7 +5,7 @@ public class Test1 {
 		
 	String hungry = "Yes";
 	
-	if(hungry == "yes")
+	if(hungry == "yes" || hungry == "Y"  || hungry == "y")
 	     System.out.println("eat something.....");
 	  else {
 		 System.out.println("Do work...");
