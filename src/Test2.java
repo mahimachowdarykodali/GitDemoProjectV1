dc01e5fe217bc9f9b22d81a1388f07699712f99e
Mahima <mahimachowdary587@gmail.com>
2023-12-31 13:58:09 -0600

    Commiting Test2 to BranchX

---
+++ src/Test2.java
@@ -0,0 +1,17 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+	String hungry = "Yes";
+	
+	if(hungry == "yes")
+	     System.out.println("eat something.....");
+	  else {
+		 System.out.println("Do work...");
+	  }
+
+	}
+
+}
+ 
